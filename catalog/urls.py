129509da3430bfd1f1aa90a6d9459992b2833831
dichenk <dchenk@DESKTOP-R5OCG73.localdomain>
2023-01-20 17:57:17 +0300

    done

---
+++ catalog/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from catalog.apps import CatalogConfig
+from catalog.views import home
+from catalog.views import contacts
+
+app_name = CatalogConfig.name
+
+
+
+urlpatterns = [
+        path('', contacts)
+]
